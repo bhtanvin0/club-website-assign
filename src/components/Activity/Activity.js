2ebe9869c39354aa2eab2c111014edc6c1e7759c
https://github.com/bhtanvin0 <bhtanvin1836@gmail.com>
2022-09-29 22:56:45 +0600

    lots of things added

--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -4,50 +4,79 @@ import Work from '../Work/Work';
 
 
 const Activity = () => {
-    const[works, setworks] = useState([])
+    const [works, setworks] = useState([])
+    const [time,setTimeSet]=useState(0)
+    function setTime (e){
+        const times=e.target.innerText
+        setTimeSet(times)
+    }
 
-    useEffect( ()=> {
-        fetch ('fakedb.json')
-        .then(res=>res.json() )
-        .then(data => setworks(data))
+
+    useEffect(() => {
+        fetch('fakedb.json')
+            .then(res => res.json())
+            .then(data => setworks(data))
     }, []);
 
-
-    const handleAddToProfile =(work)=>{
-        console.log(work)
+    const [menits, setmenits] = useState(0)
+    const handleAddToProfile = (work) => {
+        const total = menits + work.Time
+        console.log(total)
+        setmenits(total)
     }
 
     return (
 
-            
-     <div className='activity-container'>
-            
-        <div>
-            <div>
-            <div className='header'>
-                <img className='img' src="https://inlikeme.com/wp-content/uploads/2019/05/Messages-Image458805902.jpeg" alt="" />
-                <h2>DtRA-Active-club</h2>
-            </div>
-            <h4>Nowadays Some HardWord</h4>
-            </div>
-            
-            <div className="works-container">
-            {
-                works.map(work=> <Work 
-                    key={work.id}
-                    work={work}
-                    handleAddToProfile={handleAddToProfile}
-                    ></Work>)
-            }
-            </div>
-            
-        </div>
-        <div className="cart-container">
-            <h3>Cart Container</h3>
-        </div>
-    </div>
 
-        
+        <div className='activity-container'>
+
+            <div>
+                <div>
+                    <div className='header'>
+                        <img className='img' src="https://inlikeme.com/wp-content/uploads/2019/05/Messages-Image458805902.jpeg" alt="" />
+                        <h2>DtRA-Active-club</h2>
+                    </div>
+                    <h4>Nowadays Some HardWord</h4>
+                </div>
+
+                <div className="works-container">
+                    {
+                        works.map(work => <Work
+                            key={work.id}
+                            work={work}
+                            handleAddToProfile={handleAddToProfile}
+                        ></Work>)
+                    }
+                </div>
+
+            </div>
+            <div className=" mt-5 bg-amber-600 p-3">
+                <h3>Add A Break</h3>
+                <div className='flex mx-3 '>
+                    <button className='mx-1 bg-cyan-500 p-2 text-xl'> <span onClick={(e)=>setTime(e)}>10</span>s</button>
+                    <button className='mx-1 bg-cyan-500 p-2 text-xl'> <span onClick={(e)=>setTime(e)}>20</span>s</button>
+                    <button className='mx-1 bg-cyan-500 p-2 text-xl'> <span onClick={(e)=>setTime(e)}>30</span>s</button>
+                    <button className='mx-1 bg-cyan-500 p-2 text-xl'> <span onClick={(e)=>setTime(e)}>40</span>s</button>
+                    <button className='mx-1 bg-cyan-500 p-2 text-xl'> <span onClick={(e)=>setTime(e)}>50</span>s</button>
+                </div>
+                <h3> Exercise Details</h3>
+                <div>
+                    <h5>Exercise time{time}</h5>
+                   
+                </div>
+                <div>
+                    <h5>Break time :{menits}</h5>
+                    
+                </div>
+
+                <button>Activity Completed</button>
+                
+            </div>
+
+        </div>
+
+
+
     );
 };
 
